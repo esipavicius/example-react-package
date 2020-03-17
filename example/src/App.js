befa4c940e4974b1987944206b236806e735c9e8
ernestas <Ernest@hosting.review>
2020-03-17 11:50:44 +0200

    asd

--- example/src/App.js
+++ example/src/App.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react'
 
 import ExampleComponent from 'example-react-package'
 
+const ExampleComponent2 = ExampleComponent.ExampleComponent;
+
 export default class App extends Component {
   render () {
+    console.log(ExampleComponent.generate_php());
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <ExampleComponent2 text='Modern React component module' />
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,8 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import ExampleComponent from './example/ExampleComponent';
 
-import styles from './styles.css'
-
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
-  }
-
-  render() {
-    const {
-      text
-    } = this.props
-
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
-  }
+export default {
+  generate_php: () => {
+    return 'ssss'
+  },
+  ExampleComponent: ExampleComponent
 }
